d8cfacfc5ba3ce0d9e7abd517d72841619c03e51
Davit <your.email@example.com>
2024-02-14 10:37:23 +0400

    change main page and products filter select way

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     // Fetch data from Node.js server
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8000/getToken', {
+        const response = await fetch('https://www.holtrinity.com/getToken', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
